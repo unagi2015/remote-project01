4386e395b1bde798de0773b1d4de5b07a71cc135
Lee <Lee@10.4.176.219>
2018-02-25 10:26:10 +0900

    second

--- workSpaceProjec/src/workSpaceProjec/Entrance.java
+++ workSpaceProjec/src/workSpaceProjec/Entrance.java
@@ -3,8 +3,7 @@ package workSpaceProjec;
 public class Entrance {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World");
-		System.out.println("Hellow Update");
+		System.out.println("Hellow World!");
 
 
 	}
---
+++ workSpaceProjec/src/workSpaceProjec/Utils.java
@@ -0,0 +1,13 @@
+package workSpaceProjec;
+
+public class Utils {
+	
+	String strAgu = null;
+	Utils(String strAgu){
+		this.strAgu = strAgu;
+		
+	}
+	
+	
+
+}
